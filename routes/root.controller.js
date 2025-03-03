41f1569ac90805b0df9897ef17a354e9d41467f8
萝卜 <82516087+Turnip1202@users.noreply.github.com>
2025-03-03 17:05:50 +0800

    feat(错误处理，动态扫描目录)
    ---全局的错误处理
    ---监控input事件，动态扫描目录的内容
    ---引入loadsh，使用其防抖函数，对input事件高频触发进行防抖

--- routes/root.controller.js
+++ routes/root.controller.js
@@ -3,11 +3,22 @@
 
 // 模拟用户数据库
 const usrPwds = require('../db/db.js')
+const { getPostDate } = require("../utils")
 
 
 
 // 添加简单的认证中间件
 module.exports = async function (fastify, opts) {
-
+  const currentTime = getPostDate(new Date())
+  //匹配不到的路由，返回错误页面
+  fastify.setNotFoundHandler((request, reply) => {
+    reply.status(404)
+    reply.view('template/error.ejs', {
+      code: 404,
+      message: 'Not Found',
+      user: "Turnip1202",
+      currentTime,
+    })
+  })
 
 }
